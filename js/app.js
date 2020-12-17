4da2292019d1d278103214cc6713c80940a782c4
michael <mikey.m.russell@gmail.com>
2020-12-16 18:33:52 -0800

    fixed random img generation

--- js/app.js
+++ js/app.js
@@ -5,34 +5,64 @@ var myContainer = document.getElementById('imgBox');
 var imageOneElement = document.getElementById('imgOne');
 var imageTwoElement = document.getElementById('imgTwo');
 var imageThreeElement = document.getElementById('imgThree');
+var maxClicks = 5;
+var maxViews = 0;
 
-class ProductImg {
-  constructor(name, src = 'src') {
-    this.name = name;
-    this.src = `img/${name}.${'jpg'}`;
-    allImages.push(this);
-    //what do i do with this src ?????????
-  }
+
+function ProductImg(name, src = 'jpg') {
+  this.name = name;
+  this.src = `img/${name}.${src}`;
+  this.views = 0;
+  this.votes = 0;
+  allImages.push(this);
 }
 
+
 new ProductImg('bag');
+new ProductImg('banana');
+new ProductImg('bathroom');
+new ProductImg('boots');
+new ProductImg('breakfast');
+new ProductImg('bubblegum');
+new ProductImg('chair');
+new ProductImg('cthulhu');
+new ProductImg('dog-duck');
+new ProductImg('dragon');
+new ProductImg('pen');
+new ProductImg('pet-sweep');
+new ProductImg('scissors');
+new ProductImg('shark');
+new ProductImg('sweep', 'png');
 new ProductImg('tauntaun');
+new ProductImg('unicorn');
+new ProductImg('usb', 'gif');
+new ProductImg('water-can');
 new ProductImg('wine-glass');
 
 function getRandomIndex(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
+
 function renderImages() {
   var imageOneIndex = getRandomIndex(allImages.length);
   var imageTwoIndex = getRandomIndex(allImages.length);
-  var imageThreeIndex = getRandomIndex(allImages.length);
-  while (imageOneIndex < imageTwoIndex) {
+
+
+  while (imageOneIndex === imageTwoIndex) {
     imageTwoIndex = getRandomIndex(allImages.length);
   }
+  var imageThreeIndex = getRandomIndex(allImages.length);
+  while (imageThreeIndex === imageTwoIndex || imageThreeIndex === imageOneIndex) {
+    imageThreeIndex = getRandomIndex(allImages.length);
+  }
+
+
+
+
   imageOneElement.src = allImages[imageOneIndex].src;
   imageOneElement.alt = allImages[imageOneIndex].name;
   imageOneElement.title = allImages[imageOneIndex].name;
-  allImages[imageOneIndex].view++;
+  allImages[imageOneIndex].views++;
 
   imageTwoElement.src = allImages[imageTwoIndex].src;
   imageTwoElement.alt = allImages[imageTwoIndex].name;
@@ -44,16 +74,19 @@ function renderImages() {
   imageThreeElement.title = allImages[imageThreeIndex].name;
   allImages[imageTwoIndex].views++;
 
-  function handleClick(event) {
-    var clickedImage = event.target.title;
-    console.log(clickedImage);
 
-    for (var i = 0; i < allImages.length; i++)
-      if (clickedImages === allImages[i].name) {
-        allImages[i].votes++;
-      }
-  }
-  renderImages();
-  myContainer.addEventListener('click', handleClick);
 }
 renderImages();
+
+myContainer.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  var clickedImage = event.target.title;
+  console.log(clickedImage);
+
+  for (var i = 0; i < allImages.length; i++) {
+    if (clickedImage === allImages[i].name) {
+      allImages[i].votes++;
+    }
+  } renderImages();
+}
